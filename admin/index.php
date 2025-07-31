76ede49ef7f4d06a0b89b571a6ee27f4a71eb7a7
Nay Chi Cho <naychicho.dev@gmail.com>
2025-07-31 11:40:06 +0630

    -Php server working now and tested one file

---
+++ admin/index.php
@@ -0,0 +1,6 @@
+<?php
+    $batch = 111;
+    $name = "IMC"; //string data type
+    $temp = 32.5;
+    echo "<b><em>Hello Batch$batch, $name.<em></b>, welcome to ecommerce module and projects";
+?>
